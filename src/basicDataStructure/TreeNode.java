a016855418c5e955985f9a5fcc5b1195b39c6848
牛客77960967号 <lth888@88.com>
2021-04-25 11:30:13 +0800

    0425

---
+++ src/basicDataStructure/TreeNode.java
@@ -0,0 +1,46 @@
+package basicDataStructure;
+
+/**
+ * @Classname TreeNode
+ * @Description Definition for a binary tree node.
+ * @Date 2021/4/25 11:22
+ * @Created by Fabric
+ */
+
+
+public class TreeNode {
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode() {}
+    public TreeNode(int val) { this.val = val; }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ src/leetcode_897/Soluiton.java
@@ -0,0 +1,22 @@
+package leetcode_897;
+import basicDataStructure.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @Classname Soluiton
+ * @Description TODO
+ * @Date 2021/4/25 11:19
+ * @Created by Fabric
+ */
+public class Soluiton {
+    public static void main(String[] args) {
+        Deque<TreeNode> cache = new ArrayDeque<TreeNode>();
+        TreeNode root = new TreeNode();
+        int [] nums = {5,3,6,2,4,-1,8,1,-1,-1,-1,7,9};
+        root.setVal(nums[0]);
+
+
+    }
+}
